de0a587ce8859e4e244e080521af004fa976c711
fadlisabdadinov <fadlisabdad@yahoo.com>
2026-05-21 14:34:13 +0700

    Delete rest-server client autentikasi js directory

--- rest-server client autentikasi js/src/config/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql2");
-const dbPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "express_mysql",
-});
-module.exports = dbPool.promise();
--- rest-server client autentikasi js/src/middleware/logs.js
+++
@@ -1,6 +0,0 @@
-const logRequest = (req, res, next) => {
-  console.log("terjadi request ke path", req.path);
-  next();
-};
-
-module.exports = logRequest;
